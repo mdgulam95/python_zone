7b72978499c074b8a22fe10f6cc46424f050226b
Mohammed Gulam <93987639+mdgulam95@users.noreply.github.com>
2021-12-02 22:22:13 +0530

    Add files via upload

---
+++ statements/ifelif.py
@@ -0,0 +1,10 @@
+print('program started')
+name=input('enter the name')
+password=input('enter the password')
+if password=='root':
+    print('hello coder')
+elif password=='rooot':
+    print('hi coder had dinner')
+else:
+    print('oye name guest')
+print('program ended')
