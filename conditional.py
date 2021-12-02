587e07226c5af2d0c00638764ad8711dd630089a
Mohammed Gulam <93987639+mdgulam95@users.noreply.github.com>
2021-12-02 22:12:24 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,7 @@
+print('program started')
+name=input('enter the name')
+if name=='gulam':
+    print('hello gulam a nice day')
+else:
+    print('hello guest')
+print('program ended')
---
+++ if.py
@@ -0,0 +1,5 @@
+print('program started')
+name=input('enter the name')
+if name =='gulam':
+    print('hii gulam')
+print('program ended')
